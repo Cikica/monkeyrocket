3b737d2195fc72f7c7899c3cf26a21dfd91c04ee
Cikica <aleksandar.winter@gmail.com>
2014-05-13 18:40:15 +0100

    BANANAS

--- cli/monkey.py
+++ cli/monkey.py
@@ -18,17 +18,26 @@ class Monkey:
 		go_or_no_go = "if_go" if response in do['proceed'] else "if_no_go"
 		getattr(self, do[go_or_no_go]['action'] )( do[go_or_no_go]['give'] )
 
-	def install_package(self, package):
-		print "instaling"
+	def add_package(self, package):
+		print package
 
+	def get_known_and_active_packages_list (self):
+		known_packages_file = open("/vagrant/bananas/known_packages.json", "r")
+		active_packages_file = open("/vagrant/bananas/active_packages.json", "r")
+		packages = { 
+			"known" : json.loads(known_packages_file.read()),
+			"active" : json.loads(active_packages_file.read())
+		}
+		known_packages_file.close()
+		active_packages_file.close()
+		return packages
 
 	def parse_the_instructions_and_return_action_definition(self, do):
-		
-		known_packages_file = open("/vagrant/bananas/known_packages.json", "r")
-		known_packages = json.loads(known_packages_file.read())
-		known_packages_file.close()
+
+		packages = self.get_known_and_active_packages_list()
 		is_package_installed = os.path.exists("/vagrant/bananas/"+ do['<package_name>'] )
-		is_known_package = known_packages.has_key(do['<package_name>'])
+		is_known_package = packages['known'].has_key(do['<package_name>'])
+		is_active_package = do['<package_name>'] in packages['active']
 		definition = {
 			"action" : "",
 			"give"   : ""
@@ -47,9 +56,9 @@ class Monkey:
 					"say"     : "The package "+ do['<package_name>'] +" is not a banana; but is know know to me. Shall i get it? (Yes/No)",
 					"proceed" : ["yes", "Y", "Yes", "y"],
 					"if_go"   : {
-						"action" : "install_package", 
+						"action" : "add_package", 
 						"give"   : {
-							"url"    : known_packages.get(do['<package_name>']),
+							"url"    : packages['known'].get(do['<package_name>']),
 							"name"   : do['<package_name>']
 						}
 					},
@@ -59,3 +68,9 @@ class Monkey:
 					}
 				}
 			}
+
+		if do['setup'] == True and is_package_installed == False and is_known_package == True:
+			print "add package and then set it up"
+
+		if do['setup'] == True and is_package_installed == True:
+			print "setup package"
