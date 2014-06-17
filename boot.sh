7d67f1db8e31cde99f717ad9f018a7e5058d03b1
Cikica <aleksandar.winter@gmail.com>
2014-06-17 13:57:49 +0100

    Removed __init__.py from the ingore list

--- boot.sh
+++ boot.sh
@@ -7,12 +7,3 @@ apt-get clean
 pip install docopt
 
 find /vagrant/cli -exec dos2unix '{}' \;
-
-# cp -r /vagrant/cli /home/monkey_cli
-# cd /home/monkey_cli
-# python setup.py sdist
-# cd dist
-# tar -xvzf monkey-1.0.tar.gz
-# cd monkey-1.0
-# python setup.py install
-# cp /vagrant/cli/monkey /usr/local/bin
